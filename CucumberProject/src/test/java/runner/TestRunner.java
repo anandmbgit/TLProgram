a328265ef6eacfe4ac24b449fe63b5e2fc17a01a
129866866 <129866866@DESKTOP-UUUPUF0.home>
2021-11-10 21:32:39 +0000

    Third Commit

--- CucumberProject/src/test/java/runner/TestRunner.java
+++ CucumberProject/src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/Features",
-		glue= {"StepDefinitions"},
+		glue= {"StepDefinitions","Hooks"},
 				//tags= "@AddEmployeeTest", 
 		plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 				
